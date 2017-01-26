58adeea3d5041c66e610ff6f408b2bcc1829aad1
Thorin Butler <thorin.butler@gmail.com>
2017-01-25 20:39:49 -0600

    created togglePlay function that allows the user to toggle the video on and off.  Fun.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
 /* Get our elements */
 const player = document.querySelector('.player');
-/* Build our functions */
+const video = player.querySelector(".viewer");
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
+/* Build our functions */
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 /* Hook up our event listeners */
