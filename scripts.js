bf7d75d412bb4b07e3960bb7dc8801f2625b39be
Thorin Butler <thorin.butler@gmail.com>
2017-01-28 21:23:15 -0600

    progress bar updating

--- scripts.js
+++ scripts.js
@@ -36,11 +36,13 @@ function handleRangeUpdate() {
 
 function handleProgress() {
   const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`
 }
 /* Hook up our event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
