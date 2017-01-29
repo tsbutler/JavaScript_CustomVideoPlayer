c996e94b2398e802f8fcaf9d346052a43912e376
Thorin Butler <thorin.butler@gmail.com>
2017-01-28 21:32:15 -0600

    scrubbing working

--- scripts.js
+++ scripts.js
@@ -39,8 +39,9 @@ function handleProgress() {
   progressBar.style.flexBasis = `${percent}%`
 }
 
-function scrub() {
-  
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
 }
 /* Hook up our event listeners */
 video.addEventListener('click', togglePlay);
@@ -51,3 +52,4 @@ toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+progress.addEventListener('click', scrub);
